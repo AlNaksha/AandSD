9af9d00826c3da554603aa456f689261770912fc
Farnakeev Alex <Lexity@yandex.ru>
2021-08-16 13:57:00 +0300

    Create Les9 branch

--- src/Les9.c
+++ src/Les9.c
@@ -4,12 +4,35 @@
 //---------------------------------------------------------------
 
 #include <stdio.h>
-// #include "../sourse/arrays.h"
 
 //---------------------------------------------------------------
 
+int BubbleSort(int **arr, int n, int m){ 		// Пузырьковая сортировка двумерного массива
+	
+	int len = n*m;
+	
+	for (int j = 0; j < len; j++){
+
+		for (int i = 0; i < len-1; i++){
+
+			if ( *(arr + i) > *(arr+i+1) )
+				swap( &*(arr+i), &*(arr+i+1) );
+		}
+	}
+
+	return 0;
+}
+
+
 int main(int args, const char** argv){
-
-
+	
+	int Array[][] = {
+		{3,11,4,1},
+		{2,4,5,21},
+		{8,6,46,24},
+		{246,2,4,7}
+	};
+	int len = sizeof(Array)/sizeof(int);
+	
 	return 0;
 }
